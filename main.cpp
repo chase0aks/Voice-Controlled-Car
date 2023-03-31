f995404f441304fc73431e6e671ada30b31c61ba
Chase Oaks <chaseoaks14@gmail.com>
2023-03-31 08:51:07 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-cout << "Hello World";
-return 0;}
+int main()
+{
+    cout << "Hello World";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
